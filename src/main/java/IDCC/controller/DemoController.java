c16765649670f0de9c9d0eab18c4a74c0c9d679c
1406936162@qq.com <1406936162@qq.com>
2020-10-27 16:47:40 +0800

    上传第一次逆向工程生成的bean、mapper

--- src/main/java/IDCC/controller/DemoController.java
+++ src/main/java/IDCC/controller/DemoController.java
@@ -1,6 +1,10 @@
 package IDCC.controller;
 
+import IDCC.bean.Account;
+import IDCC.mapper.AccountMapperIml;
 import io.swagger.annotations.Api;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +16,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
  */
 @Controller
 public class DemoController {
+
+    AccountMapperIml accountMapperIml;
     /**
      * @description: 
      * @return: 返回界面html的文件名，控制界面跳转
@@ -32,5 +38,23 @@ public class DemoController {
     @RequestMapping("/index")
     public String index(){
         return "index";
+    }
+
+    /**
+     * @description: 测试数据库的增删改查
+     * @return: 无
+     * @author: Lai Zhouhao
+     * @time: 2020/10/27 16:20
+     */
+    @Test
+    public void Test(){
+        Account account = new Account();
+        account.setAccountId(1);
+        account.setAccountPwd("679914");
+        account.setAccountRight("1");
+        System.out.println(account.getAccountPwd());
+        accountMapperIml.insert(account);
+        System.out.println("插入数据成功");
+        System.out.println("插入失败");
     }
 }
